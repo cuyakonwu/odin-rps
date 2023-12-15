148e5e3cc9fe7d06e38f55053e4155e75712daa1
cuyakonwu <cuyakonwu@uchicago.edu>
2023-12-15 16:41:51 -0500

    Properly prevented invalid inputs during rps, and changed the backend logic to index without incrementing

--- javascript.js
+++ javascript.js
@@ -8,22 +8,21 @@ playerWins = 0
 computerWins = 0
 const selections = ["rock", "paper", "scissors"]
 
-function getComputerChoice (){
-    choice = Math.floor(Math.random() * 3) + 1
-    return choice
-}
-
 function playRound (playerSelection) {
-    computerSelection = getComputerChoice()
-
+    computerSelection = Math.floor(Math.random() * 3)
     rps(playerSelection, computerSelection)
 }
 
 function game () {
     while (playerWins < 5 && computerWins < 5){
         console.log(playerSelection = prompt("rock, paper, or scissors"))
-        playRound(selections.indexOf(playerSelection) + 1)
-        console.log("Player: " + playerWins + " Computer: " + computerWins)
+        if (selections.includes(playerSelection)){
+            playRound(selections.indexOf(playerSelection))
+            console.log("Player: " + playerWins + " Computer: " + computerWins)
+        }
+        else{
+            console.log("Please enter a valid input")
+        }
     }
     if (playerWins > computerWins){
         console.log("Congrats You Won!")
@@ -35,10 +34,10 @@ function game () {
 
 function rps (firstPick, secondPick) {
 
-    if (firstPick == 1 && secondPick == 3) {
+    if (firstPick == 0 && secondPick == 2) {
         playerWins++
     }
-    else if (firstPick == 3 && secondPick == 1) {
+    else if (firstPick == 2 && secondPick == 0) {
         computerWins++
     }
     else if (firstPick > secondPick) {
