17d372b6576447d87973c8dfc867ddbf806b9886
cuyakonwu <cuyakonwu@uchicago.edu>
2023-12-15 22:42:40 -0500

    Finished unpolished logic for rps including playable buttons and win screen

--- javascript.js
+++ javascript.js
@@ -1,49 +1,61 @@
 /*
-1 is rock
-2 is paper
-3 is scissors
+0 is rock
+1 is paper
+2 is scissors
 */
+const selections = ["rock", "paper", "scissors"];
+let playerWins = 0;
+let computerWins = 0;
 
-playerWins = 0
-computerWins = 0
-const selections = ["rock", "paper", "scissors"]
+const rockBtn = document.querySelector("#rockBtn");
+rockBtn.addEventListener('click', function() {
+    playRound(0);
+});
+
+const paperBtn = document.querySelector("#paperBtn");
+paperBtn.addEventListener('click', function() {
+    playRound(1);
+});
+
+const scissorsBtn = document.querySelector("#scissorsBtn");
+scissorsBtn.addEventListener('click', function() {
+    playRound(2);
+});
+
 
 function playRound (playerSelection) {
-    computerSelection = Math.floor(Math.random() * 3)
-    rps(playerSelection, computerSelection)
+    computerSelection = Math.floor(Math.random() * 3);
+    rps(playerSelection, computerSelection);
+    updateScores();
 }
 
-function game () {
-    while (playerWins < 5 && computerWins < 5){
-        console.log(playerSelection = prompt("rock, paper, or scissors"))
-        if (selections.includes(playerSelection)){
-            playRound(selections.indexOf(playerSelection))
-            console.log("Player: " + playerWins + " Computer: " + computerWins)
-        }
-        else{
-            console.log("Please enter a valid input")
-        }
-    }
-    if (playerWins > computerWins){
-        console.log("Congrats You Won!")
-    }
-    else {
-        console.log("Refresh The Page to Try Again")
+function rps(firstPick, secondPick) {
+    if (
+        (firstPick === 0 && secondPick === 2) ||
+        (firstPick === 1 && secondPick === 0) ||
+        (firstPick === 2 && secondPick === 1)
+    ) {
+        playerWins++;
+    } else if (
+        (secondPick === 0 && firstPick === 2) ||
+        (secondPick === 1 && firstPick === 0) ||
+        (secondPick === 2 && firstPick === 1)
+    ) {
+        computerWins++;
     }
 }
 
-function rps (firstPick, secondPick) {
-
-    if (firstPick == 0 && secondPick == 2) {
-        playerWins++
-    }
-    else if (firstPick == 2 && secondPick == 0) {
-        computerWins++
-    }
-    else if (firstPick > secondPick) {
-        playerWins++
-    }
-    else{
-        computerWins++
+function updateScores() {
+    const pScore = document.querySelector('#pScore');
+    const cScore = document.querySelector('#cScore');
+    const winMsg = document.createElement("div");
+    pScore.textContent = `Player: ${playerWins}`;
+    cScore.textContent = `Computer: ${computerWins}`;
+    if (playerWins == 5){
+        winMsg.textContent = "Congrats You Won!";
+        (document.querySelector("body")).appendChild(winMsg);
+    } else if (computerWins == 5){
+        winMsg.textContent = "Refresh The Page to Try Again";
+        (document.querySelector("body")).appendChild(winMsg);
     }
 }
